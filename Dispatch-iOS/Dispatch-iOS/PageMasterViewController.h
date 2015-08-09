ff2cace788bafefa95a16af02bf821736074e6f6
rsbballguy <rsbballguy@gmail.com>
2015-08-08 23:56:56 -0400

    Finished tutorial with pageviewcontroller

---
+++ Dispatch-iOS/Dispatch-iOS/PageMasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  PageMasterViewController.h
+//  Dispatch-iOS
+//
+//  Created by Rahul Sundararaman on 8/8/15.
+//  Copyright (c) 2015 Rahul Sundararaman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PageMasterViewController : UIPageViewController
+
+@end
---
+++ Dispatch-iOS/Dispatch-iOS/PageMasterViewController.m
@@ -0,0 +1,37 @@
+//
+//  PageMasterViewController.m
+//  Dispatch-iOS
+//
+//  Created by Rahul Sundararaman on 8/8/15.
+//  Copyright (c) 2015 Rahul Sundararaman. All rights reserved.
+//
+
+#import "PageMasterViewController.h"
+
+@interface PageMasterViewController ()
+
+@end
+
+@implementation PageMasterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
